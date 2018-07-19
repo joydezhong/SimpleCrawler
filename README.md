f663917b3a58770746794d376256e389b1047b0f
zwp <1347034022@qq.com>
2018-07-19 12:30:50 +0800

    Add save data to MongoDB

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 # SimpleCrawler
-简易的Crawler程序，获取页面数据。
+简易的Crawler程序，获取页面数据，写入json文件，并保存到MongoDB。
 
-程序依赖两个npm包：`superagent `、`cheerio`
+程序依赖三个npm包：`superagent `、`cheerio`、`mongoose`，和nodejs的`fs`模块
 
-> superagent 是一个轻量的,渐进式的ajax api,可读性好,学习曲线低,内部依赖nodejs原生的请求api,适用于nodejs环境下
-> cheerio是nodejs的抓取页面模块，为服务器特别定制的，快速、灵活、实施的jQuery核心实现。适合各种Web爬虫程序。相当于node.js中的jQuery
+确保本地安装mongoDB数据库，默认连接地址：localhost:27017，可安装可视化工具Robo 3T更加直观的查看数据。
+
+> superagent---是一个轻量的,渐进式的ajax api,可读性好,学习曲线低,内部依赖nodejs原生的请求api,适用于nodejs环境下
+>
+> cheerio---是nodejs的抓取页面模块，为服务器特别定制的，快速、灵活、实施的jQuery核心实现。适合各种Web爬虫程序。相当于node.js中的jQuery
 
 主要文件：crawler.js
 
 `npm install` 安装依赖
 
-`node crawler.js` 运行程序，刷新项目目录，打开boss.json即可查看获取的数据
+`node crawler.js` 运行程序，刷新项目目录，打开newBooks.json即可查看获取的数据；或者打开可视化工具，刷新数据库集合，也可以查看；或者到MongoDB安装的bin目录下执行`mongo`连接数据库查看。
--- crawler.js
+++ crawler.js
@@ -3,48 +3,76 @@ const http = require("http");
 const path = require("path");
 const url = require("url");
 const fs = require("fs");
+const mongoose = require("mongoose");
 
 const superagent = require("superagent");
 const cheerio = require("cheerio");
 
+//连接本地mongodb数据库Douban
+var mongourl = 'mongodb://localhost/Douban';
+mongoose.connect(mongourl);
+var Schema = mongoose.Schema;
+//创建模型
+var newBookSchema = new Schema({
+    title: String,
+    bookId: Number,
+    grade: String,
+    bookInfo: String,
+    description: String
+});
+var NewBook = mongoose.model('NewBook', newBookSchema);
+
 superagent
-    .get("https://www.zhipin.com/job_detail/?city=100010000&source=10&query=%E5%89%8D%E7%AB%AF")
+    .get("https://book.douban.com/latest?icn=index-latestbook-all")
     .end((error, response)=>{
+
         //获取页面文档数据
         var content = response.text;
+
         //cheerio也就是node下的jquery 将整个文档包装成一个集合，定义一个$接收
         var $ = cheerio.load(content);
+
         //定义一个空数组，用来接收数据
         var result = [];
+
         //分析文档结构 先获取每个li 再遍历里面的内容（此时每个li里面就存放着我们想要获取的数据）
-        $(".job-list li .job-primary").each(function(index,value){
-            //地址和类型为一行显示，要用到字符串截取
-            //地址
-            let address = $(value).find(".info-primary").children().eq(1).html();
-            //类型
-            let type = $(value).find(".info-company p").html();
-            //解码
-            address  = unescape(address.replace(/&#x/g, '%u').replace(/;/g, ''));
-            type = unescape(type.replace(/&#x/g, '%u').replace(/;/g, ''));
-            //字符串截取
-            let addressArr = address.split('<em class="vline"></em>');
-            let typeArr = type.split('<em class="vline"></em>');
+        $(".cover-col-4 li").each(function(index,value){
+
+            //提取url链接中的id
+            var address = $(value).find(".cover").attr("href");
+            var bookId = address.replace(/[^0-9]/ig,"");
+
             //将获取的数据以对象的形式添加到数组中
-            result.push({
-                title: $(value).find(".name .job-title").text(),
-                money: $(value).find(".name .red").text(),
-                address: addressArr,
-                company: $(value).find(".info-company a").text(),
-                type: typeArr,
-                position: $(value).find(".info-publis .name").text(),
-                txImg: $(value).find(".info-publis img").attr("src"),
-                time: $(value).find(".info-publis p").text()
+            var oneBook = {
+                title: $(value).find(".detail-frame h2 a").text(),
+                bookId: bookId,
+                grade: $(value).find(".detail-frame .rating .font-small").text().replace(/\ +/g,"").replace(/[\r\n]/g,""),
+                bookInfo: $(value).find(".detail-frame .color-gray").text().replace(/\ +/g,"").replace(/[\r\n]/g,""),
+                bookImg: $(value).find(".cover img").attr("src"),
+                description: $(value).find(".detail-frame .detail").text().replace(/\ +/g,"").replace(/[\r\n]/g,"")
+            };
+            result.push(oneBook);
+
+            //将每个书本信息实例化到newBook模型中
+            var newBook = new NewBook(oneBook);
+
+            //保存到mongodb
+            newBook.save(function(err){
+               if(err){
+                   console.log('保存失败：'+ err);
+                   return;
+               }
+               console.log("OJBK!");
             });
+
         });
+
         //将数组转成字符串
         result = JSON.stringify(result);
+
         //将数组输出到json文件里 刷新目录  即可看到当前文件夹多出一个boss.json文件
-        fs.writeFile("boss.json", result, "utf-8", (error)=>{
+        fs.writeFile("newBooks.json", result, "utf-8", (error)=>{
+
             //监听错误，如正常输出，则打印null
             if(error == null){
                 console.log("恭喜您，数据爬取成功!请打开json文件");
